e18170128e8e7738a0fecfcc3cb64d1fcfae3d12
Pablo Armando Mac Beath Milian <pabloarmando.macbeath@gmail.com>
2025-04-02 14:32:30 -0600

    Header padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
     <header className="fixed top-0 left-0 w-full bg-slate-900 text-white py-4 px-8 shadow-lg z-50">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
-        <h1 className="text-2xl font-bold">
+        <h1 className="text-2xl md:pl-50 font-bold">
           Hack<span className="text-blue-400">Puebla0111</span>
         </h1>
 
